07405934f3cb56e70ad2c4c76044783c817343d5
Yunjae Jeong <yunjae2295@naver.com>
2020-05-12 03:21:25 +0900

    adding folder

---
+++ src/Screen/Login/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Styled from 'styled-components/native';
+
+const Container = Styled.SafeAreaView`
+flex: 1;
+background-color: #FFFFFF;
+`;
+
+const Login = () => {
+    return (
+        <Container>
+            
+        </Container>
+    );
+};
+
+export default Login;
